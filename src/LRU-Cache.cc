37e42da6106ead2c5238522a9d47c044d9048600
wangqs1990 <283648304@qq.com>
2014-05-22 14:12:19 +0800

    lru cache  with unorderedmap and list

--- src/LRU-Cache.cc
+++ src/LRU-Cache.cc
@@ -5,10 +5,6 @@
 *set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
 */
 
-#include <list>
-#include <unordered_map>
-#include <iostream>
-using namespace std;
 typedef list<pair<int, int>>::iterator KeyIter;
 
 class LRUCache{
@@ -28,42 +24,29 @@ public:
       return -1;
     } else {
       value = iter->second->second;
-      this->clist.erase(iter->second);
-      this->clist.push_back(make_pair(key, value));
+      //this->clist.erase(iter->second);
+      this->clist.splice(this->clist.begin(), this->clist, iter->second);
+      this->cmap[key] = this->clist.begin();
       return value;
     }
   }
 
   void set(int key, int value) {
     pair<int, int> *keyIter;
-    if (this->clist.size() == capacity) {
-      keyIter = &this->clist.front();
-      
-      this->cmap.erase(keyIter->first);
-      clist.pop_front();
-      
+    unordered_map<int, KeyIter>::iterator iter = this->cmap.find(key);
+
+    if (iter != this->cmap.end()) {
+      //update
+      iter->second->second = value;
+      this->clist.splice(this->clist.begin(), this->clist, iter->second);
+    } else {
+      if (this->clist.size() == capacity) {
+        keyIter = &this->clist.back();
+        this->cmap.erase(keyIter->first);
+        clist.pop_back();
+      }
+      this->clist.push_front(make_pair(key, value));
     }
-    this->cmap[key] = this->clist.insert(this->clist.end(), make_pair(key, value));
+    this->cmap[key] = this->clist.begin();
   }
 };
-
-int main(int argc, char *argv[]) {
-  /* unordered_map<int, int> m;
-  m.insert(make_pair(1,10));
-  m.insert(make_pair(2,20));
-  m.insert(make_pair(3,30));
-  m.insert(make_pair(4,40));
-
-  cout<<(*m.find(10)).second<<endl;*/
-
-  LRUCache l(2);
-  l.set(2, 20);
-  
-  l.set(1, 10);
-
-  cout<<l.get(2)<<endl;
-  l.set(3, 30);
-
-  cout<<l.get(2)<<endl;
-  system("pause");
-}
